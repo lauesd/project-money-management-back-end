07ddb39607fa302fd84cb6c365838d4b7aa502f4
Jose Gabriel Espinosa Organista <jespinosa@grupoasd.com>
2025-04-01 12:17:18 -0500

    La historia de usuario income y expense ha sido mejorada con exito

--- src/main/java/com/project_money_management_back_end/Budget/model/Budget.java
+++ src/main/java/com/project_money_management_back_end/Budget/model/Budget.java
@@ -1,11 +1,15 @@
 package com.project_money_management_back_end.budget.model;
 
+import com.project_money_management_back_end.transaction.model.Transaction;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -19,4 +23,6 @@ public class Budget {
     @Column(nullable = false)
     private String encrypted_amount;
 
+    @OneToMany(mappedBy = "budget") // Relación bidireccional
+    private List<Transaction> transactions;
 }
